9c5ee12436b19870063734bd0ad7d85f5005d4c5
Ryan Hirsch <Ryan.Hirsch@gmail.com>
2013-10-15 05:47:00 -0500

    Updated to try and use serialport, still no dice :(

--- index.js
+++ index.js
@@ -4,14 +4,38 @@ Arduino D2 	= RFID Enable
 Arduino GND = RFID GND
 Arduino 5v 	= RFID VCC
 */
-var five = require('johnny-five'),
-	board = new five.Board(),
+var	j5 = require('johnny-five'),
+	board = new j5.Board(),
 	rfid;
 
-board.on('ready', function() {
-	new five.pin('D2').low();
-	rfid = new five.Sensor({
-    pin: 'D0',
-    freq: 2400
-  });
+//board.on('ready', function() {
+//	new j5.pin('D2').low();
+//	sp.open(function () {
+//		console.log('open');
+//		sp.on('data', function(data) {
+//			console.log('data received: ' + data);
+//		});  
+//		sp.write("ls\n", function(err, results) {
+//			console.log('err ' + err);
+//			console.log('results ' + results);
+//		});  
+//	});
+//});
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort; // localize object constructor
+
+var sp = new SerialPort("/dev/tty-usbserial1", { 
+  parser: serialport.parsers.readline("\r") ,
+  baudrate: 2400
+}, false);
+
+sp.open(function () {
+  console.log('open');
+  sp.on('data', function(data) {
+    console.log('data received: ' + data);
+  });  
+  sp.write("ls\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });  
 });
